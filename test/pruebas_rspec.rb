ba746066d43ddb09ec78e6327b8fb49e48259912
GUillermo <alu0100702149@ull.edu.es>
2013-11-08 14:33:37 +0000

    Actualizado test

--- test/pruebas_rspec.rb
+++ test/pruebas_rspec.rb
@@ -48,6 +48,32 @@ describe Matriz do
 
 	it "Multiplicacion" do 
 	
+	m = Matriz.new(2,2)
+	a = Fraccion.new(1,2)
+	m.insertar(0,0, a)
+	b = Fraccion.new(1,3)
+	m.insertar(0,1, b)
+	c = Fraccion.new(1,4)
+	m.insertar(1,0, c)
+	d = Fraccion.new(1,5)
+	m.insertar(1,1, d)
+
+	m1 = Matriz.new(2,2)
+	e = Fraccion.new(1,2)
+	m1.insertar(0,0, e)
+	f = Fraccion.new(2,3)
+	m1.insertar(0,1, f)
+	g = Fraccion.new(3,4)
+	m1.insertar(1,0, g)
+	h = Fraccion.new(4,5)
+	m1.insertar(1,1, h)
+	aux = Matriz.new(2,2)
+	aux = m.multi(m1)
+
+	aux[0][0].should eq(Fraccion.new(1,2))
+	aux[0][1].should eq(Fraccion.new(3,5))
+	aux[1][0].should eq(Fraccion.new(11,40))
+	aux[1][1].should eq(Fraccion.new(49,150))
 	
 	end
 	
